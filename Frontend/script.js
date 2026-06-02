f78cf4186558727048d12ca1873a79d2a837b459
ManjeetMahto445 <mahtomanjeet445@gmail.com>
2026-06-03 03:10:21 +0530

    added button village area and footer and linked with script.js

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,9 +1,19 @@
+console.log("Script loaded successfully!");
 document
 .getElementById("cropForm")
 .addEventListener("submit", function(e){
 
     e.preventDefault();
 
-    alert("AI crop recommendation coming soon 🚀");
+    const state = document.getElementById("state").value;
+    const district = document.getElementById("district").value;
+    const village = document.getElementById("village").value;
+    const area = document.getElementById("area").value;
+
+
+    console.log("State:", state);
+    console.log("District:", district);
+    console.log("Village:", village);
+    console.log("Area:", area);
 
 });
